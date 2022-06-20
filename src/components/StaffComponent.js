4b0a085a97af1b79fb312876a297821b4f8632e1
maimanhtuong <77782513+maimanhtuong@users.noreply.github.com>
2022-06-20 10:49:53 +0700

    commit5

--- src/components/StaffComponent.js
+++ src/components/StaffComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { STAFFS,DEPARTMENTS } from "./staffs.jsx";
-import { Card, CardTitle, CardBody,CardHeader } from "reactstrap";
+import { Card, CardTitle, CardBody,Navbar,NavbarText,NavbarBrand,Collapse,Nav,UncontrolledDropdown,DropdownMenu,DropdownItem } from "reactstrap";
 import dateFormat from "dateformat";
 class Staff extends Component {
   constructor() {
@@ -39,6 +39,56 @@ class Staff extends Component {
     return (
       <>
         <div className="container">
+        <div>
+  <Navbar
+    color="light"
+    expand="md"
+    light
+  >
+    <NavbarBrand href="/">
+     Ứng dụng quản lý nhân viên v1.0
+    </NavbarBrand>
+    <Collapse navbar>
+      <Nav
+        className="me-auto"
+        navbar
+      >
+        
+        <UncontrolledDropdown
+          inNavbar
+          nav
+        >
+          <DropdownToggle
+            caret
+            nav
+          >
+            Số cột hiển thị
+          </DropdownToggle>
+          <DropdownMenu right>
+            <DropdownItem>
+               1 Cột
+            </DropdownItem>
+            <DropdownItem>
+               2 Cột
+            </DropdownItem>
+            <DropdownItem>
+               3 Cột
+            </DropdownItem>
+            <DropdownItem>
+               6 Cột
+            </DropdownItem>
+            
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </Nav>
+      <NavbarText>
+        Simple Text
+      </NavbarText>
+    </Collapse>
+  </Navbar>
+</div>
+
+          ////////////////////////////
           <div className="row">
             {staff}
             </div>
