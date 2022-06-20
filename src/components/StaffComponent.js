c3db82e1fa607edd6bba689f4616dbd1c932d4ce
maimanhtuong <77782513+maimanhtuong@users.noreply.github.com>
2022-06-20 11:34:51 +0700

    commit6

--- src/components/StaffComponent.js
+++ src/components/StaffComponent.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
-import { STAFFS,DEPARTMENTS } from "./staffs.jsx";
-import { Card, CardTitle, CardBody,Navbar,NavbarText,NavbarBrand,Collapse,Nav,UncontrolledDropdown,DropdownMenu,DropdownItem } from "reactstrap";
+import { STAFFS } from "./staffs.jsx";
+import {
+  Card,
+  CardTitle,
+  CardBody,
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  Nav,
+  UncontrolledDropdown,
+  DropdownMenu,
+  DropdownItem,
+  DropdownToggle,
+} from "reactstrap";
+
 import dateFormat from "dateformat";
 class Staff extends Component {
   constructor() {
     super();
-    this.state = { staffs: STAFFS, detail: null };
+    this.state = { staffs: STAFFS, detail: null, columns: "col-sm-2" };
   }
 
   detailStaff(staff) {
@@ -13,9 +26,15 @@ class Staff extends Component {
       detail: (
         <Card>
           <CardBody>
-            <CardTitle>Họ và tên: <h3>{staff.name}</h3></CardTitle>
-            <CardTitle>Ngày sinh: {dateFormat(staff.doB,"dd,mm,yyyy")}</CardTitle>
-            <CardTitle>Ngày vào công ty: {dateFormat(staff.startDate,"dd,mm,yyyy")}</CardTitle>
+            <CardTitle>
+              Họ và tên: <h3>{staff.name}</h3>
+            </CardTitle>
+            <CardTitle>
+              Ngày sinh: {dateFormat(staff.doB, "dd,mm,yyyy")}
+            </CardTitle>
+            <CardTitle>
+              Ngày vào công ty: {dateFormat(staff.startDate, "dd,mm,yyyy")}
+            </CardTitle>
             <CardTitle>Phòng ban: {staff.department.name}</CardTitle>
             <CardTitle>Số ngày nghĩ còn lại: {staff.annualLeave}</CardTitle>
             <CardTitle>Số ngày đã làm thêm: {staff.overTime}</CardTitle>
@@ -28,7 +47,13 @@ class Staff extends Component {
   render() {
     const staff = this.state.staffs.map((staff) => {
       return (
-        <Card key={staff.id} onClick={() => this.detailStaff(staff)} className="m-3">
+        <Card
+          key={staff.id}
+          onClick={() => this.detailStaff(staff)}
+          className={this.state.columns}
+          
+
+        >
           <CardBody>
             <CardTitle tag="h5">{staff.name}</CardTitle>
           </CardBody>
@@ -39,60 +64,45 @@ class Staff extends Component {
     return (
       <>
         <div className="container">
-        <div>
-  <Navbar
-    color="light"
-    expand="md"
-    light
-  >
-    <NavbarBrand href="/">
-     Ứng dụng quản lý nhân viên v1.0
-    </NavbarBrand>
-    <Collapse navbar>
-      <Nav
-        className="me-auto"
-        navbar
-      >
-        
-        <UncontrolledDropdown
-          inNavbar
-          nav
-        >
-          <DropdownToggle
-            caret
-            nav
-          >
-            Số cột hiển thị
-          </DropdownToggle>
-          <DropdownMenu right>
-            <DropdownItem>
-               1 Cột
-            </DropdownItem>
-            <DropdownItem>
-               2 Cột
-            </DropdownItem>
-            <DropdownItem>
-               3 Cột
-            </DropdownItem>
-            <DropdownItem>
-               6 Cột
-            </DropdownItem>
-            
-          </DropdownMenu>
-        </UncontrolledDropdown>
-      </Nav>
-      <NavbarText>
-        Simple Text
-      </NavbarText>
-    </Collapse>
-  </Navbar>
-</div>
+          
+            <div>
+              <Navbar color="info" expand="md" light>
+                <NavbarBrand href="/">
+                  Ứng dụng quản lý nhân viên v1.0
+                </NavbarBrand>
+                <Collapse navbar>
+                  <Nav className="me-auto" navbar>
+                    <UncontrolledDropdown inNavbar nav>
+                      <DropdownToggle caret nav>
+                        Số cột hiển thị
+                      </DropdownToggle>
+                      <DropdownMenu right>
+                        <DropdownItem
+                          onClick={() => (this.setState({ columns: "col-sm-12" }))}
+                        >
+                          1 Cột
+                        </DropdownItem>
+                        <DropdownItem
+                          onClick={() => (this.setState({ columns: "col-sm-6" }))}
+                        >2 Cột</DropdownItem>
+                        <DropdownItem
+                         onClick={() => (this.setState({ columns: "col-sm-4" }))}
+                        >3 Cột</DropdownItem>
+                        <DropdownItem
+                          onClick={() => (this.setState({ columns: "col-sm-2" }))}
+                        >6 Cột</DropdownItem>
+                      </DropdownMenu>
+                    </UncontrolledDropdown>
+                  </Nav>
+                </Collapse>
+              </Navbar>
+            </div>
+          
 
-          ////////////////////////////
           <div className="row">
             {staff}
-            </div>
-         <h3> Nhấp vào để xem chi tiết nhân viên</h3>
+          </div>
+          <h3> Nhấp vào để xem chi tiết nhân viên</h3>
           {this.state.detail}
         </div>
       </>
