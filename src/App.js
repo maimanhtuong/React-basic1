7766be1b00bb8fa27a8ca3c82e5ea95114ede317
maimanhtuong <77782513+maimanhtuong@users.noreply.github.com>
2022-06-21 17:31:48 +0700

    commit2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import Menu from './components/MenuComponent';
+import {Navbar,NavbarBrand} from 'reactstrap';
+import { DISHES } from './shared/dishes';
+import { Component } from 'react';
 
-function App() {
+class App extends Component{
+  constructor() {
+    super();
+    this.state = { dishes: DISHES };
+  }
+
+  render() {
   return (
      <div >
-      <Menu></Menu>
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristore Con Fusoin</NavbarBrand>
+      </div>
+      </Navbar>
+      <Menu dishes={this.state.dishes}></Menu>
       </div>
     
   );
+}
 }
 
 export default App;
