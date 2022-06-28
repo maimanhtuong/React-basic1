7dbcda2142dad1b3aab2153abc883ff268627284
maimanhtuong <77782513+maimanhtuong@users.noreply.github.com>
2022-06-28 20:58:03 +0700

    commit9

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGoogle } from "@fortawesome/free-solid-svg-icons";
+
 
 function Footer() {
   return (
